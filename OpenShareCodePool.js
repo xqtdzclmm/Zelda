26e0c42131866a5791b262ed62f5053c2135b5e9
pokbrin <pokbrin>
2021-12-15 11:45:17 +0800

    finish pool codes

--- OpenShareCodePool.js
+++ OpenShareCodePool.js
@@ -2,27 +2,42 @@ const https = require('https')
 
 
 function OpenShareCodePool() {
-    pullCodes = async function (env, randomNum = 20) {
-        if (process.env.POOL_URL) {
-            return new Promise(resolve => {
-                const prefix = `${this.hostname}/${this.env}`
-                const url = this.restful ? `${prefix}/${this.randomNum}` : `${prefix}?num=${this.randomNum}`
-                console.log(`请求地址:${url}`)
-                let localCodes = eval(`process.env.${this.env}`)
-                localCodes = localCodes && localCodes.indexOf('@') > -1 ? codes.split('@') : []
+    this.pullCodes = async function (env, restful, randomNum = 20) {
+        return new Promise(resolve => {
+            let mergeCodes = [];
+            if (env) {
+                let localCodes = eval(`process.env.${env}`)
+                mergeCodes = localCodes && localCodes.indexOf('@') > -1 ? codes.split('@') : []
+            }
+
+            const poolUrl = process.env.POOL_URL
+            if (poolUrl) {
+                const prefix = `${poolUrl}/${env}`
+                const url = restful ? `${prefix}/${randomNum}` : `${prefix}?num=${randomNum}`
                 const req = https.get(url, res => {
                     res.on('data', r => {
                         let shareCodesRes = JSON.parse(r);
-                        shareCodesRes.data = [...new Set(shareCodesRes.data), ...localCodes]
+                        console.log(`从池子中下发助力码:${url}\n =========================>${r}\n <=========================`)
+                        shareCodesRes.data = [...new Set(shareCodesRes.data), ...mergeCodes]
                         resolve(shareCodesRes)
                     })
                 })
                 req.end()
-            })
-        }
+            }
+            resolve({code: 200, data: mergeCodes})
+        })
 
     },
-        log = function (ptPin) {
+        this.log = function (ptPin, env, code) {
+            console.log('====================================')
+            console.log(`用户:${ptPin},${env}当前助力码:${code}`)
+            info = {
+                ptPin: ptPin,
+                env: env,
+                code: code
+            }
+            console.log(`${JSON.stringify(info)}`)
+            console.log('====================================')
 
         }
 
